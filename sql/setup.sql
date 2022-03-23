d5f428be550a6ba90445d4681ab010dc0230231a
I183213 <jandeilson.nascimento@bradesco.com.br>
2022-03-23 15:10:03 -0300

    Adicionado docker do banco de dados sql server

---
+++ sql/setup.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE db_desafio_dev;
+GO
+
+USE db_desafio_dev;
+GO
+
+CREATE TABLE tb_cnab
+(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    tipo SMALLINT NOT NULL,
+    data DATETIME NOT NULL,
+    valor VARCHAR(10),
+    cpf VARCHAR(11),
+    cartao VARCHAR(12),
+    hora VARCHAR(6),
+    dono_da_loja VARCHAR(14),
+    nome_loja VARCHAR(19)
+)
