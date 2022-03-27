dcb836bfc340dee5a783154a607c9fba59717a64
I183213 <jandeilson.nascimento@bradesco.com.br>
2022-03-27 14:10:05 -0300

    Adicionado configuracao CORs

--- srv-cnab/src/config/router/Router.ts
+++ srv-cnab/src/config/router/Router.ts
@@ -2,12 +2,18 @@ import express from "express";
 import CNABController from "../../api/CNABController";
 import swaggerUi from "swagger-ui-express";
 import specs from "../../swagger/Swagger";
+import cors from "cors";
 
 const router = express.Router();
 
 const options = {
     explorer: true
 };
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
 
 // SRV API Documentation
 router.use("/api-docs", swaggerUi.serve);
@@ -18,7 +24,7 @@ router.get("/health", (req, res) => res.send("OK"));
 
 const cnabController = new CNABController();
 
-router.post("/cnab", cnabController.upload);
-router.get("/cnab", cnabController.getAll);
+router.post("/cnab", cors(corsOptions), cnabController.upload);
+router.get("/cnab", cors(corsOptions), cnabController.getAll);
 
 export = router;
